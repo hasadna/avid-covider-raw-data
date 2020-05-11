5b78513ee30e0c1b090f1c9b1de4ceefb6709fd5
Adam Kariv <adam.kariv@gmail.com>
2020-05-11 16:35:32 +0300

    More city ranking

---
+++ tools/city_images.py
@@ -0,0 +1,57 @@
+from math import sin, cos, sqrt, atan2, radians, log
+import json
+import requests
+from common import geo_file, upload_file
+
+def distance(lat1, lon1, lat2, lon2):
+    # approximate radius of earth in km
+    R = 6373.0
+
+    lat1 = radians(lat1)
+    lon1 = radians(lon1)
+    lat2 = radians(lat2)
+    lon2 = radians(lon2)
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    return R * c * 1000.0
+
+def center(lat1, lon1, lat2, lon2):
+    return (lat1 + lat2)/2, (lon1 + lon2)/2
+
+def extent(lats, lons):
+    return (min(lats), min(lons), max(lats), max(lons))
+
+def split_coordinates(coordinates):
+    return [x[1] for x in coordinates], [x[0] for x in coordinates]
+
+def zoomlevel(size, lat1, lon1, lat2, lon2):
+    c_lat, c_lon = center(lat1, lon1, lat2, lon2)
+    dist = max(distance(c_lat, lon1, c_lat, lon2),
+               distance(lat1, c_lon, lat2, c_lon))
+    scaled = dist / cos(radians(c_lat))
+    level = 78271.484 / scaled * size
+    print(dist, scaled, level)
+    return min(log(level)/log(2), 20)
+
+def static_image_url(lat, lng, zoom, width, height):
+    return f'https://api.mapbox.com/styles/v1/wios/ck9spo9jo02p71ip3m6xfchxk/static/' +\
+           f'{lng},{lat},{zoom},0/{width}x{height}@2x' +\
+           f'?access_token=pk.eyJ1Ijoid2lvcyIsImEiOiJjazh4ZXZ6Z24wejdtM3JvN2F1MHdlc2Z4In0.vz7knGcRWWGE4LGOLx8c7g'
+
+def upload_static_image(id, width=600, height=400):
+    features = json.load(open(geo_file('cities')))
+    feature = next(filter(lambda f: f['properties']['id'] == id, features['features']))
+    coords = split_coordinates(feature['geometry']['coordinates'][0])
+    bbox = extent(*coords)
+    ctr = center(*bbox)
+    size = width, height
+    zoom = zoomlevel(min(size) * 1.1, *bbox)
+    path = 'data/city_preview_{}.png'.format(id)
+    url = static_image_url(*ctr, zoom, *size)
+    upload_file(requests.get(url).content, path)
+    return '/' + path
--- tools/city_ranking.py
+++ tools/city_ranking.py
@@ -22,10 +22,10 @@ if __name__ == '__main__':
             date=r['date'].isoformat(), sr=float(r['symptoms_ratio_weighted']), nr=int(r['num_reports_weighted']))
         ),
         DF.concatenate(dict(
-            id=[], score_date=[]
+            id=[], city_name=[], score_date=[]
         ), target=dict(name='ranking')),
         DF.join_with_self('ranking', '{city_name}', dict(
-            id=None, scores=dict(name='score_date', aggregate='array')
+            id=None, city_name=None, scores=dict(name='score_date', aggregate='array')
         )),
         DF.filter_rows(lambda r: r['scores'][-1]['nr'] >= 200),
         DF.add_field('sortkey', 'integer', lambda r: int(r['scores'][-1]['sr'] * 1000000) + r['scores'][-1]['nr']),
@@ -33,7 +33,7 @@ if __name__ == '__main__':
         DF.delete_fields(['sortkey']),
         DF.add_field('rank', 'integer', 0),
         DF.add_field('translations', 'object', lambda r: city_translations[r['city_name']]),
-        DF.add_field('image_url', 'string', lambda r: upload_static_image(r['id']))
+        DF.add_field('image_url', 'string', lambda r: upload_static_image(r['id'])),
         ranker(),
     ).results()
     rankings = r[0]
