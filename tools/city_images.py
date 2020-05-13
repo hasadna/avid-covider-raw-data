0db9ee38a8aae478aca75e1d1cb5624a0e12d7b6
Adam Kariv <adam.kariv@gmail.com>
2020-05-13 11:56:10 +0300

    Even more debugging prints

--- tools/city_images.py
+++ tools/city_images.py
@@ -48,9 +48,11 @@ def zoomlevel(size, lat1, lon1, lat2, lon2):
     return min(log(level)/log(2), 20)
 
 def static_image_url(lat, lng, zoom, width, height):
-    return f'https://api.mapbox.com/styles/v1/wios/ck9spo9jo02p71ip3m6xfchxk/static/' +\
-           f'{lng},{lat},{zoom},0/{width}x{height}@2x' +\
-           f'?access_token=pk.eyJ1Ijoid2lvcyIsImEiOiJjazh4ZXZ6Z24wejdtM3JvN2F1MHdlc2Z4In0.vz7knGcRWWGE4LGOLx8c7g'
+    ret = f'https://api.mapbox.com/styles/v1/wios/ck9spo9jo02p71ip3m6xfchxk/static/' +\
+          f'{lng},{lat},{zoom},0/{width}x{height}@2x' +\
+          f'?access_token=pk.eyJ1Ijoid2lvcyIsImEiOiJjazh4ZXZ6Z24wejdtM3JvN2F1MHdlc2Z4In0.vz7knGcRWWGE4LGOLx8c7g'
+    print('static image url:', ret)
+    return ret
 
 def upload_static_image(id, width=600, height=400):
     features = json.load(open(geo_file('cities')))
