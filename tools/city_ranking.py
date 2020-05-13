02b4d0f0c4b1bfd6921fdba6c92ad4440b49e40c
Adam Kariv <adam.kariv@gmail.com>
2020-05-13 09:14:33 +0300

    Remove prints

--- tools/city_ranking.py
+++ tools/city_ranking.py
@@ -55,7 +55,6 @@ if __name__ == '__main__':
         DF.add_field('sr', 'number', lambda r: r['ws']/r['nr']),
         DF.delete_fields(['ws']),
         DF.sort_rows('{date}'),
-        DF.printer(),
     ).results()
 
     national = dict(
