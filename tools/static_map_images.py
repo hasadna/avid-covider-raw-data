655ec65507195467a1938022677c3b601a59113f
Adam Kariv <adam.kariv@gmail.com>
2020-05-13 10:57:38 +0300

    More prints

--- tools/static_map_images.py
+++ tools/static_map_images.py
@@ -3,9 +3,11 @@ from common import upload_file
 
 
 def static_image_url(lat, lng, zoom, width, height):
-    return f'https://api.mapbox.com/styles/v1/wios/ck9spo9jo02p71ip3m6xfchxk/static/' +\
-           f'{lat},{lng},{zoom},0/{width}x{height}@2x' +\
-           f'?access_token=pk.eyJ1Ijoid2lvcyIsImEiOiJjazh4ZXZ6Z24wejdtM3JvN2F1MHdlc2Z4In0.vz7knGcRWWGE4LGOLx8c7g'
+    ret = f'https://api.mapbox.com/styles/v1/wios/ck9spo9jo02p71ip3m6xfchxk/static/' +\
+          f'{lat},{lng},{zoom},0/{width}x{height}@2x' +\
+          f'?access_token=pk.eyJ1Ijoid2lvcyIsImEiOiJjazh4ZXZ6Z24wejdtM3JvN2F1MHdlc2Z4In0.vz7knGcRWWGE4LGOLx8c7g'
+    print(ret)
+    return ret
 
 
 def create_preview_image():
